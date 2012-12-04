59c9af084aed279a17deb069e256deb04a54b846
Kyle Derkacz <kyle@saymedia.com>
2012-12-04 09:15:48 -0800

    Incrementing the version number to include the AngularJS change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='slashstatic',
-    version='0.1.0',
+    version='0.1.2',
     description='A library of common static assets in a Django application.',
     author='Kyle Derkacz',
     author_email='slashstatic@kylederkacz.com',
